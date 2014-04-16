6e05e3c115afe0401863fedc330579b91c323f25
Patrick Allen <prallen90@gmail.com>
2014-04-16 15:00:02 -0400

    Add basic palette

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,8 +1,28 @@
 'use strict';
 
+var palArray = [
+    '#000000',
+    '#B21F35',
+    '#D82735',
+    '#FF7435',
+    '#FFA135',
+    '#FFCB35',
+    '#FFF735',
+    '#00753A',
+    '#009E47',
+    '#16DD36',
+    '#0052A5',
+    '#0079E7',
+    '#06A9FC',
+    '#681E7E',
+    '#7D3CB5',
+    '#BD7AF6'
+];
+
 var paintState = false;
 var paintColor = '#000000';
 
+// Bind mouse events for painting 
 $('table').on('mousedown', 'td', function(){
     paintState = true;
     $(this).css('background', paintColor);
@@ -18,6 +38,13 @@ $('html').on('mouseup', function(){
     paintState = false;
 });
 
+// Bind mouse events for palette
+$('ul').on('click', 'li', function(){
+    var $this = $(this);
+    $this.parent().find("li").filter('.active-palette').removeClass('active-palette');
+    $this.addClass('active-palette');
+    paintColor = $this.data('color');
+});
 
 
 // Function that draws a blank table
@@ -34,4 +61,22 @@ function drawTable(){
     $('table').html(tableHtml);
 }
 
+// Function that creates the color palette
+function createPalette(palArray){
+    var paletteHTML = '';
+
+    for (var x=0; x < palArray.length; x++){
+        paletteHTML += '<li data-color = '+ palArray[x] + '></li>';
+    }
+    $('ul').html(paletteHTML);
+    $('li').each(function(){
+        var $this = $(this);
+        var $color = $this.data('color');
+        $this.css('background-color', $color);
+    });
+    $('li').first().addClass('active-palette');
+}
+
+
 drawTable();
+createPalette(palArray);
